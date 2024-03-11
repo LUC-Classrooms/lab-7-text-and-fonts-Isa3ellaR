12c42232e7fdda439f2d8f44f77e35347ad2c852
Isabella Rodriguez <157334109+Isa3ellaR@users.noreply.github.com>
2024-03-11 06:33:25 +0000

    Lab 7 complete and submitted

--- script.js
+++ script.js
@@ -4,14 +4,23 @@
 
 function setup() {
   createCanvas(640, 240);
-  textFont("Arial");
+  textFont("Palatino");
 }
 
 function draw() {
-  background(200);
+  background(0);
+  fill(255)
   textSize(16);
-  text("one small step for a man ...", 20, 60);
-  textSize(32);
-  text("one giant leap for mankind.", 20, 160);
+  text("Press the mouse for a little joke...", 20, 30)
+  textSize(25)
+  text("How does a banana answer the phone??", 20, 90);
+
+  if(mouseIsPressed){
+    textSize(32);
+    fill(252, 227, 3)
+    text("YELLOW?!", 20, 160);
+  }
+ 
+  
 
 }
